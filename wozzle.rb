3a72fb7a5923b4f02e4ea933f336e3ce90ff70fa
Noah B. Sakane <100352806+NoahBSakane@users.noreply.github.com>
2022-02-25 10:59:55 +0900

    fix url & hash

--- wozzle.rb
+++ wozzle.rb
@@ -1,8 +1,8 @@
 class Wozzle < Formula
   desc "wozzle: Wordle-like word guessing game, playable in the terminals!"
   homepage "https://github.com/NoahBSakane/homebrew-wozzle"
-  url "https://github.com/NoahBSakane/wozzle/archive/refs/tags/v1.0.tar.gz"
-  sha256 "85da44b0d51750edc2441982713bbb68272faa115bb8f1baaf258d7420ad8624"
+  url "https://github.com/NoahBSakane/wozzle/releases/download/v1.0/wozzle.tar.gz"
+  sha256 "a0be17115a29a6edfade6fc693cf221c7a391100b84a50c2b310ed0a999e5462"
   license "MIT"
 
   def install
